b3fa4f90a78bfbe950332e67a37855632bf75653
Alyona Yedinorozhik <misterrpandaa@gmail.com>
2025-06-24 23:38:40 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/DataEntryTest.java
@@ -0,0 +1,53 @@
+package ru.netology.rest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DataEntryTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void positiveEntryForm() {
+        List<WebElement> inputs = driver.findElements(By.cssSelector("inputs"));
+        inputs.get(0).sendKeys("Иван Иванов");
+        inputs.get(1).sendKeys("+79999999999");
+        driver.findElement(By.className("checkbox__control")).click();
+        driver.findElement(By.className("button__content")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+}
